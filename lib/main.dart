8eae97814169860d1500fbc2b9c49be09a8547f3
HaiMap <haimap@HaiMaps-MacBook-Pro.local>
2021-07-25 13:04:15 +0700

    Login page, home page, undefined page

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_db/constants/routing.dart';
+import 'router.dart' as router;
 
 import 'homepage.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Food App',
-      home: FoodApp(),
+      onGenerateRoute: router.generateRoute,
+      initialRoute: LoginPageRoute,
     ));
-
-class FoodApp extends StatelessWidget {
-  const FoodApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final widthCustom = MediaQuery.of(context).size.width;
-    final heightCustom = MediaQuery.of(context).size.height;
-    return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green[800],
-            title: Center(
-              child: Text(
-                'My Food App',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          body: SingleChildScrollView(
-            child: Container(
-              width: widthCustom * 1,
-              height: heightCustom * 1,
-              color: Colors.black,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: heightCustom * 0.13),
-                  Container(
-                    height: widthCustom * 0.9,
-                    width: widthCustom * 0.9,
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://i.pinimg.com/736x/a8/d1/9b/a8d19bfb6d5172adc87d65908c69137a.jpg'),
-                        ),
-                        borderRadius: BorderRadius.circular(180)),
-                  ),
-                  SizedBox(
-                    height: 80,
-                  ),
-                  Container(
-                      decoration: BoxDecoration(
-                          color: Colors.green[800],
-                          borderRadius: BorderRadius.circular(60)),
-                      child: TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage()));
-                          },
-                          child: Text(
-                            'GetStar',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.bold),
-                          ))),
-                ],
-              ),
-            ),
-          )),
-    );
-  }
-}
---
+++ lib/router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal_db/constants/routing.dart';
+import 'package:flutter_meal_db/screens/home_page.dart';
+import 'package:flutter_meal_db/screens/login_page.dart';
+import 'package:flutter_meal_db/screens/undefined_page.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HomePageRoute:
+      return MaterialPageRoute(builder: (builder) => HomePage());
+    case LoginPageRoute:
+      return MaterialPageRoute(builder: (builder) => LoginPage());
+    default:
+      return MaterialPageRoute(builder: (builder) => UndefinedPage());
+  }
+}
