f426ab6b2e01c727dd441c33e296e98498809234
DESKTOP-EC68343\Na Ngo <ngopona@gmail.com>
2021-07-25 13:56:19 +0700

    ! move to correct possition

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,11 +10,98 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final widthCustom = MediaQuery.of(context).size.width;
+    final heightCustom = MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor, // Colors.green[800],
+      home: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              width: widthCustom * 1,
+              height: heightCustom * 1,
+              padding: EdgeInsets.all(10),
+              color: Colors.black,
+              child: Column(children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Find Your',
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Favorite Food',
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 60,
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  child: Row(
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        height: 60,
+                        width: 300,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              hintText: 'What Do You Want Oder ?'),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.control_point),
+                            color: Colors.white,
+                            iconSize: 30,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ]),
+            ),
+          ),
         ),
       ),
     );
