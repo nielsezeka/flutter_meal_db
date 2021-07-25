b819337ab6cd5bf83ec5236c4f90ea30b6ceff9d
DESKTOP-5TS0NTS\Dung Nguyen <nguyenducdung.09t6a@gmail.com>
2021-07-25 13:29:41 +0700

    update

---
+++ lib/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final widthCustom = MediaQuery.of(context).size.width;
+    final heightCustom = MediaQuery.of(context).size.height;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              width: widthCustom * 1,
+              height: heightCustom * 1,
+              padding: EdgeInsets.all(10),
+              color: Colors.black,
+              child: Column(children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Find Your',
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Favorite Food',
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 60,
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  child: Row(
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        height: 60,
+                        width: 300,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              hintText: 'What Do You Want Oder ?'),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.control_point),
+                            color: Colors.white,
+                            iconSize: 30,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'homepage.dart';
+import 'home_page.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -42,35 +42,41 @@ class FoodApp extends StatelessWidget {
                     height: widthCustom * 0.9,
                     width: widthCustom * 0.9,
                     decoration: BoxDecoration(
-                        color: Colors.black,
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://i.pinimg.com/736x/a8/d1/9b/a8d19bfb6d5172adc87d65908c69137a.jpg'),
-                        ),
-                        borderRadius: BorderRadius.circular(180)),
+                      color: Colors.black,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://i.pinimg.com/736x/a8/d1/9b/a8d19bfb6d5172adc87d65908c69137a.jpg'),
+                      ),
+                      borderRadius: BorderRadius.circular(180),
+                    ),
                   ),
                   SizedBox(
                     height: 80,
                   ),
                   Container(
-                      decoration: BoxDecoration(
-                          color: Colors.green[800],
-                          borderRadius: BorderRadius.circular(60)),
-                      child: TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage()));
-                          },
-                          child: Text(
-                            'GetStar',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.bold),
-                          ))),
+                    decoration: BoxDecoration(
+                      color: Colors.green[800],
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'GetStar',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
